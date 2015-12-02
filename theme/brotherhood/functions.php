17fc58fd04b5459e87694f3f49a1a16d29f97cc6
mobigliani <matta@interia.pl>
2015-12-02 09:43:45 +0100

    Fix previous/next post navigation in the same category.

--- theme/brotherhood/functions.php
+++ theme/brotherhood/functions.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * Brotherhood theme functions and definitions
  *
@@ -107,28 +107,25 @@ add_action( 'after_setup_theme', 'brotherhood_custom_header_setup', 11 );
 
 
 /**
- * Display navigation to next/previous set of posts when applicable and
- * in the same category .
+ * Display navigation to next/previous post when applicable, in the same category.
  *
  */
-function twentythirteen_paging_nav() {
-	global $wp_query;
+function twentythirteen_post_nav() {
+	global $post;
 
-	// Don't print empty markup if there's only one page.
-	if ( $wp_query->max_num_pages < 2 )
+	// Don't print empty markup if there's nowhere to navigate.
+	$previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
+	$next     = get_adjacent_post( false, '', false );
+
+	if ( ! $next && ! $previous )
 		return;
 	?>
-	<nav class="navigation paging-navigation" role="navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'twentythirteen' ); ?></h1>
+	<nav class="navigation post-navigation" role="navigation">
+		<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'twentythirteen' ); ?></h1>
 		<div class="nav-links">
 
-			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentythirteen' ), TRUE ); ?></div>
-			<?php endif; ?>
-
-			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentythirteen' ), TRUE ); ?></div>
-			<?php endif; ?>
+			<?php previous_post_link( '%link', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', 'twentythirteen' ), TRUE ); ?>
+			<?php next_post_link( '%link', _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link', 'twentythirteen' ), TRUE ); ?>
 
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->
